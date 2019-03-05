63919bca575132657cbd7cb2f6551744038bcc05
Angela Tong <atong04@nyit.edu>
2019-03-05 18:53:18 -0500

    New Login

---
+++ .idea/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:test_flutter1/authentication.dart';
+import 'package:test_flutter1/root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
