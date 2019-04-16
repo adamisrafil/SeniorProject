a3629d6460d45eaabe39dc75bd73c09e88ef4ff7
Angela Tong <atong04@nyit.edu>
2019-04-16 19:00:21 -0400

    Forum Page Layout All Set
    Add button, going into posts, community list tiles

--- lib/forumPage.dart
+++ lib/forumPage.dart
@@ -20,6 +20,12 @@ class _ForumPageState extends State<ForumPage> {
       child: Scaffold(
           appBar: AppBar(
             title: Text('Forums Page'),
+              actions: <Widget>[
+                IconButton(
+                    tooltip: 'Search',
+                    icon: const Icon(Icons.search),
+                    onPressed: (){},
+                )],
             bottom: TabBar(tabs: [
               Tab(text: 'Feed'),
               Tab(text: 'Community',)
@@ -27,10 +33,209 @@ class _ForumPageState extends State<ForumPage> {
           ),
           body: TabBarView(
             children: [
-              PagewiseListViewExample(),
-              PagewiseListViewExample(),
+              FeedPage(),
+              CommunityPage(),
+              //put new class stateless widget here and in the new class call pagewiselistview
             ],
-          )),
+          ),
+      ),
+    );
+  }
+}
+class crudPost extends StatelessWidget {
+//final PostModel postModel;
+
+//crudPost(this.postModel);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("User Details"),
+      ),
+      body: Center(
+        child: Text(
+          "asfg",
+          //postModel.title,
+          style: TextStyle(fontSize: 35.0),
+        ),
+      ),
+    );
+  }
+}
+
+class CommunityCrudPost extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 4,
+    child: Scaffold(
+      appBar: AppBar(
+        title: Text('Forums Page'),
+        bottom: TabBar(tabs: [
+          Tab(text: 'Community',)
+        ]),
+      ),
+      body: TabBarView(
+        children: [
+          PagewiseListViewExample(),
+          //PagewiseListViewExample(),
+          //CommunityPage(),
+          //put new class stateless widget here and in the new class call pagewiselistview
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+        showDialog(
+            context: context,
+            builder: (_) => new AlertDialog(
+              title: new Text("Dialog Title"),
+              content: new Row(
+                children: <Widget> [
+                  new Expanded (
+                      child: new TextField(
+                        autofocus: true,
+                        decoration: new InputDecoration(
+                            labelText: 'Title', hintText: 'e.g asdggdf'),
+                      )
+                  ),
+                  new Expanded (
+                      child: new TextField(
+                        autofocus: true,
+                        decoration: new InputDecoration(
+                            labelText: 'Question', hintText: 'e.g asdggdf?'),
+                      )
+                  )
+                ],
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            )
+        )},
+        child: new Icon(Icons.add),
+      ),
+    )
+    );
+  }
+}
+
+class CommunityPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text('Architecture'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            Navigator.push(
+                context, new MaterialPageRoute(
+                builder: (context) => new CommunityCrudPost())
+            );
+          },
+        ),
+        ListTile(
+          title: Text('Business'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            Navigator.push(
+                context, new MaterialPageRoute(
+                builder: (context) => new CommunityCrudPost())
+            );
+          },
+        ),
+        ListTile(
+          title: Text('Communications'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            Navigator.push(
+                context, new MaterialPageRoute(
+                builder: (context) => new CommunityCrudPost())
+            );
+          },
+        ),
+        ListTile(
+          title: Text('Engineering'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            Navigator.push(
+                context, new MaterialPageRoute(
+                builder: (context) => new CommunityCrudPost())
+            );
+          },
+        ),
+        ListTile(
+          title: Text('Science'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            Navigator.push(
+                context, new MaterialPageRoute(
+                builder: (context) => new CommunityCrudPost())
+            );
+          },
+        ),
+
+      ],
+    );
+  }
+}
+
+class FeedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 4,
+      child: Scaffold(
+      body: TabBarView(
+        children: [
+          PagewiseListViewExample(),
+          //PagewiseListViewExample(),
+          //CommunityPage(),
+          //put new class stateless widget here and in the new class call pagewiselistview
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+        showDialog(
+            context: context,
+            builder: (_) => new AlertDialog(
+              title: new Text("Dialog Title"),
+              content: new Row(
+                children: <Widget> [
+                  new Expanded (
+                      child: new TextField(
+                        autofocus: true,
+                        decoration: new InputDecoration(
+                            labelText: 'Title', hintText: 'e.g asdggdf'),
+                      )
+                  ),
+                  new Expanded (
+                      child: new TextField(
+                        autofocus: true,
+                        decoration: new InputDecoration(
+                            labelText: 'Question', hintText: 'e.g asdggdf?'),
+                      )
+                  )
+                ],
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            )
+        )},
+        child: new Icon(Icons.add),
+      ),
+    )
     );
   }
 }
@@ -63,8 +268,9 @@ class PagewiseListViewExample extends StatelessWidget {
           title: Text(entry.title),
           subtitle: Text(entry.body),
         ),
-        Divider()
+        Divider(),
       ],
+
     );
   }
 }
@@ -118,23 +324,3 @@ class ImageModel {
   }
 }
 
-class crudPost extends StatelessWidget {
-//final PostModel postModel;
-
-//crudPost(this.postModel);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("User Details"),
-      ),
-      body: Center(
-        child: Text(
-          "asfg",
-          //postModel.title,
-          style: TextStyle(fontSize: 35.0),
-        ),
-      ),
-    );
-  }
-}
